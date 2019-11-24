6fe6017183e72c4de8bcc9d9033a629f90b1944f
Trubetskaia Inna <trubetskaya_inna@ukr.net>
2019-11-24 16:40:29 +0200

    added server request to get posts and comments

--- asyncAwait.js
+++ asyncAwait.js
@@ -45,10 +45,48 @@ function get (url) {
 
 button.addEventListener('click', async () => {
   wrapperUser.innerHTML = ''
-  let users = await get('https://jsonplaceholder.typicode.com/users')
+  let users = await get('https://jsonplaceholder.typicode.com/users').catch(err => console.log(err))
   users.forEach((element) => {
     const divUser = wrapperUser.appendChild(document.createElement('div'))
     divUser.className = 'users'
     divUser.innerHTML = element.name
+    divUser.addEventListener('click', async () => {
+      wrapperComments.innerHTML = ''
+      wrapperPost.innerHTML = ''
+      divUser.style.backgroundColor = 'beige'
+      let arrUser = document.getElementsByClassName('users')
+      for (let i = 0; i < arrUser.length; i++) {
+        if (i !== (element.id - 1)) {
+          arrUser[i].style.backgroundColor = 'white'
+        }
+      }
+      let posts = await get(`https://jsonplaceholder.typicode.com/posts?userId=${element.id}`).catch(err => console.log(err))
+      posts.forEach(async (element2) => {
+        const divPost = wrapperPost.appendChild(document.createElement('div'))
+        divPost.className = 'posts'
+        divPost.innerHTML = element2.title
+
+        const spinner = divPost.appendChild(document.createElement('div'))
+        spinner.className = 'spinner-border spinner-border-sm'
+        spinner.setAttribute('role', 'status')
+        const spinnerSpan = spinner.appendChild(document.createElement('span'))
+        spinnerSpan.className = 'sr-only'
+
+        let commits = await get(`https://jsonplaceholder.typicode.com/comments?postId=${element2.id}`).catch(err => console.log(err))
+        spinner.style.display = 'none'
+        const elementNumber = divPost.appendChild(document.createElement('div'))
+        elementNumber.innerHTML = commits.length
+        elementNumber.className = 'number'
+
+        divPost.addEventListener('click', async () => {
+          wrapperComments.innerHTML = ''
+          commits.forEach((element3) => {
+            const divComment = wrapperComments.appendChild(document.createElement('div'))
+            divComment.className = 'comments'
+            divComment.innerHTML = element3.name
+          })
+        })
+      })
+    })
   })
 })
